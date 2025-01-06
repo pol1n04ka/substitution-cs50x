86397adabd28ad388b99a005cb6be40be56c60ab
Polina Prokopenko <topolina1337@gmail.com>
2025-01-06 01:54:07 +0100

    working draft (w/o user input)

--- substitution.c
+++ substitution.c
@@ -4,14 +4,15 @@
 
 int PrintUsage();
 int CheckDuplicates(char *key);
+void EncryptText(char *key, char *text);
 
 int main(int argc, char *argv[]) {
+  // TODO: maybe refactor this code to more readable function.
   if (argc != 2) {
     PrintUsage();
   } else {
     char *key = argv[1];
 
-    // TODO: add check for dublicates
     if (strlen(key) != 26) {
       PrintUsage();
     } else {
@@ -24,28 +25,27 @@ int main(int argc, char *argv[]) {
         key[i] = toupper(key[i]);
       }
 
-      printf("%s\n", key);
-
       int isHaveDuplicates = CheckDuplicates(key);
 
       if (isHaveDuplicates) {
-        printf("\n");
-        printf("There is duplicates, key must have unique characters. \n");
-        printf("\n");
         PrintUsage();
       }
+
+      char str[] = "Hello, world!";
+
+      EncryptText(key, str);
     }
   }
 }
 
 int PrintUsage() {
   printf("Usage: ./substitution key\n");
-  printf("Key must be 26 characters long and contain only alfabetic "
+  printf("Key must be 26 characters long and contain only unique alfabetic "
          "characters.\n");
   return 1;
 }
 
-// Takes uppercased key value and ckecks if there is any duplicates.  
+// Takes uppercased key value and ckecks if there is any duplicates.
 // 1 means that duplicates is found, 0 means otherwise
 int CheckDuplicates(char *key) {
   for (int i = 0; i < 26; i++) {
@@ -67,5 +67,24 @@ int CheckDuplicates(char *key) {
 
 // TODO: add code for processing and encrypting text
 // output must preserve case
+void EncryptText(char *key, char *text) {
+  int textLength = (int)strlen(text);
+  char encryptedText[textLength];
 
-// TODO:
+  for (int i = 0; i <= textLength; i++) {
+    if (isalpha(text[i])) {
+      int charPosition = toupper(text[i]) - 'A';
+
+      if (islower(text[i])) {
+        encryptedText[i] = tolower(key[charPosition]);
+      } else {
+        encryptedText[i] = key[charPosition];
+      }
+    } else {
+      encryptedText[i] = text[i];
+    }
+  }
+
+  printf("%s\n", encryptedText);
+  printf("%i\n", textLength);
+}
