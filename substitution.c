517fc839fbb241d7f114187e80b948d8de8b16a3
Polina Prokopenko <topolina1337@gmail.com>
2025-01-07 01:14:22 +0100

    refactored code a little

--- substitution.c
+++ substitution.c
@@ -2,51 +2,65 @@
 #include <stdio.h>
 #include <string.h>
 
-int PrintUsage();
+void PrintUsage();
+int ValidateKey(char *key);
 int CheckDuplicates(char *key);
 void EncryptText(char *key, char *text);
 
 int main(int argc, char *argv[]) {
-  // TODO: maybe refactor this code to more readable function.
   if (argc != 2) {
     PrintUsage();
-  } else {
-    char *key = argv[1];
-
-    if (strlen(key) != 26) {
-      PrintUsage();
-    } else {
-      for (int i = 0; i < 26; i++) {
-        char character = key[i];
-
-        if (!isalpha(character))
-          PrintUsage();
-
-        key[i] = toupper(key[i]);
-      }
-
-      int isHaveDuplicates = CheckDuplicates(key);
-
-      if (isHaveDuplicates) {
-        PrintUsage();
-      }
-
-      char str[] = "Hello, world!";
-
-      EncryptText(key, str);
-    }
+    return 1;
   }
+
+  if (strcmp(argv[1], "-h") == 0) {
+    PrintUsage();
+    return 0;
+  }
+
+  char *key = argv[1];
+  if (!ValidateKey(key))
+    return 1;
+
+  char str[] = "Hello, world!";
+  EncryptText(key, str);
+
+  return 0;
 }
 
-int PrintUsage() {
+void PrintUsage() {
   printf("Usage: ./substitution key\n");
   printf("Key must be 26 characters long and contain only unique alfabetic "
          "characters.\n");
+}
+
+// Validates the key and makes sure that all characters is uppercased.
+// 1 means that all is okay. Also checks duplicates.
+int ValidateKey(char *key) {
+  if (strlen(key) != 26) {
+    PrintUsage();
+    return 0;
+  }
+
+  for (int i = 0; i < 26; i++) {
+    char character = key[i];
+
+    if (!isalpha(character)) {
+      PrintUsage();
+      return 0;
+    }
+
+    key[i] = toupper(key[i]);
+  }
+
+  if (CheckDuplicates(key))
+    return 0;
+
   return 1;
 }
 
 // Takes uppercased key value and ckecks if there is any duplicates.
-// 1 means that duplicates is found, 0 means otherwise
+// 1 means that duplicates is found, 0 means otherwise.
 int CheckDuplicates(char *key) {
   for (int i = 0; i < 26; i++) {
     char iChar = key[i];
@@ -63,13 +77,13 @@ int CheckDuplicates(char *key) {
   return 0;
 }
 
-// TODO: add code to take user input
+// TODO: add code to take user input.
 
-// TODO: add code for processing and encrypting text
-// output must preserve case
+// Encrypts text based on uppercased key.
 void EncryptText(char *key, char *text) {
   int textLength = (int)strlen(text);
   char encryptedText[textLength];
+  encryptedText[textLength] = '\0'; // Ensure that string is terminated.
 
   for (int i = 0; i <= textLength; i++) {
     if (isalpha(text[i])) {
@@ -84,7 +98,6 @@ void EncryptText(char *key, char *text) {
       encryptedText[i] = text[i];
     }
   }
-
-  printf("%s\n", encryptedText);
-  printf("%i\n", textLength);
+  
+  printf("\nEncrypted text: %s\n", encryptedText);
 }
